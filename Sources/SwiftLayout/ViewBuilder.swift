e9d870136a045047e907732602ae38b118b7f48b
jihoonahn.dev <jihoonahn.dev@gmail.com>
2024-06-04 03:12:06 +0000

    Delete Swift View and View Builder and ViewConvertable

--- Sources/SwiftLayout/ViewBuilder.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@resultBuilder public struct ViewBuilder {
-    public static func buildBlock(_ values: ViewConvertable...) -> [View] {
-        return values.asViews()
-    }
-    
-}
