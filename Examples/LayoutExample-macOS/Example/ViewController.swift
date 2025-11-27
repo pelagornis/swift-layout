91ee1150152e56fcc5445254a9bef34334c8f9ca
jihoonahn <jihoonahn.dev@gmail.com>
2025-11-28 01:35:05 +0900

    Update iOS -> iOS and macOS

---
+++ Examples/LayoutExample-macOS/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LayoutExample-macOS
+//
+//  Created by Jihoonahn on 11/28/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
